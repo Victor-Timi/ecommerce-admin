76efd98d0e7ff8ff6d28b648021920fe064b9601
Victor-Timi <vickiegenius@gmail.com>
2023-07-15 14:11:28 +0100

    second commit

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,19 @@
-import Image from 'next/image'
+"use client";
+import React, { useEffect } from "react";
 
-export default function Home() {
-  return (
-    
-    <div>Hello Admin Dashboard</div>
-    
-  )
-}
+import useStoreModal from "@/hooks/use-store-modal";
+
+const SetUpPage = () => {
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen, onOpen]);
+
+  return <div>RootPage</div>;
+};
+
+export default SetUpPage;
